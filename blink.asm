99c5ed891e595fb8b76fb43d8e6c7457b1e4fe2f
Mikhail Samoylenko <samoylenko@gmail.com>
2015-11-07 09:20:19 -0500

    1 sec blink, minor updates

--- blink.asm
+++ blink.asm
@@ -1,6 +1,8 @@
 ; Target device		: ATtiny85V
 ; Target frequency	: 1 Mhz
 
+; PB4 = LED indicator
+
 .include "atmel/tn85def.inc"
 
 ;=[ Code Segment ]======================================================
@@ -11,41 +13,46 @@
 ;=[ Interrupts Table ]==================================================
 
 	rjmp	RESET
-	reti						;	rjmp	INT0_ISR
-	reti						;	rjmp	PCINT0_ISR
-	reti						;	rjmp	TIM1_COMPA_ISR
-	reti						;	rjmp	TIM1_OVF_ISR
+	reti				;	rjmp	INT0_ISR
+	reti				;	rjmp	PCINT0_ISR
+	reti				;	rjmp	TIM1_COMPA_ISR
+	reti				;	rjmp	TIM1_OVF_ISR
 	rjmp	TIM0_OVF_ISR
-	reti						;	rjmp	EE_RDY_ISR
-	reti						;	rjmp	ANA_COMP_ISR
-	reti						;	rjmp	ADC_ISR
-	reti						;	rjmp	TIM1_COMPB_ISR
-	reti						;	rjmp	TIM0_COMPA_ISR
-	reti						;	rjmp	TIM0_COMPB_ISR
-	reti						;	rjmp	WDT_ISR
-	reti						;	rjmp	USI_START_ISR
-	reti						;	rjmp	USI_OVF_ISR
+	reti				;	rjmp	EE_RDY_ISR
+	reti				;	rjmp	ANA_COMP_ISR
+	reti				;	rjmp	ADC_ISR
+	reti				;	rjmp	TIM1_COMPB_ISR
+	reti				;	rjmp	TIM0_COMPA_ISR
+	reti				;	rjmp	TIM0_COMPB_ISR
+	reti				;	rjmp	WDT_ISR
+	reti				;	rjmp	USI_START_ISR
+	reti				;	rjmp	USI_OVF_ISR
 
 ;=[ Interrupt handlers ]================================================
 
-;-[ Timer 0 Overflow ]----------------------------------------------
+;-[ Timer0 Overflow interrupt ]-------------------------------------
 
 TIM0_OVF_ISR:
-
 	push	r16
-	in		r16, SREG
+	in	r16, SREG
 	push	r16
 
-	in		r16, PORTB
-;	ldi		r17, ( 1 << PB4 )	; r17 is always PB4
-	eor		r16, r17			; invert PB4
-	out		PORTB, r16
+	dec	r19
+	brne	TIM0_OVF_ISR_exit	; exit if r19 is not 0
 
-	pop		r16
-	out		SREG, r16
-	pop		r16
+	ldi	r19, 30
+	in	r16, PORTB
+;	ldi	r17, ( 1 << PB4 )	; r17 is always PB4
+	eor	r16, r17		; invert PB4
+	out	PORTB, r16
+
+TIM0_OVF_ISR_exit:
+
+	pop	r16
+	out	SREG, r16
+	pop	r16
+
 	reti
-
 
 ;=[ Program Start ]=====================================================
 
@@ -53,32 +60,38 @@ RESET:
 
 ;-[ Initialization ]------------------------------------------------
 
-	; init stack
-	ldi		r16, HIGH( RAMEND )
-	out		SPH, r16
-	ldi		r16, LOW( RAMEND )
-	out		SPL, r16
+	; global variables
+	ldi	r17, ( 1 << PB4 )	; r17 = PB4
+	ldi	r19, 30			; r19 = events counter for tim0ovf int	
 
-	; disable unused devices
-	; TODO: add more
-	ldi		r16, ( 1 << ACD )
-	out		ACSR, r16
+	; init stack
+	ldi	r16, HIGH( RAMEND )
+	out	SPH, r16
+	ldi	r16, LOW( RAMEND )
+	out	SPL, r16
 
 	; configure port B
-	ldi		r17, ( 1 << PORTB4 ) 	; everything but PB4 is input
-	out		DDRB, r17
-	clr		r16;
-	out		PORTB, r16		; disable pull-ups and set output to ground
+;	ldi	r17, ( 1 << PB4 ) 	; everything but PB4 is input
+	out	DDRB, r17
+	ldi	r16, ( 1 << PB3 )	; enable pull-up at PB3 and set output to ground
+	out	PORTB, r16
 
-	; init Timer 0 and enable interrupts
-	; 1Mhz / 1024 / 256 overflow, ~3.8 events, ~2 blinks / sec
-	ldi		r16, ( 1 << CS02 )|( 1 << CS00 )
-	out		TCCR0B, r16
-	ldi		r16, 1 << TOV0
-	out		TIFR, r16
-	ldi		r16, 1 << TOIE0
-	out		TIMSK, r16
-	sei
+;-[ Interrupts initialization ]-------------------------------------
+
+	; Init Timer0 Overflow Interrupt
+	; 1Mhz / 64 / 256 = 61 events / second
+
+	ldi	r16, ( 1 << CS01 )|( 1 << CS00 )	; set source internal clock with 64 prescaler mode
+	out	TCCR0B, r16
+	ldi	r16, ( 1 << TOIE0 )			; Timer/Counter0 Overflow Interrupt Enable
+	out	TIMSK, r16
+
+	; Enable interrupts
+	clr	r16					; clear interrupts
+	out	GIFR, r16
+	out	TIFR, r16
+	out	TCNT0, r16
+	sei						; enable interrupts
 
 ;-[ Main Routine ]--------------------------------------------------
 
